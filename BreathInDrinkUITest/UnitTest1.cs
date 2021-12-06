03d5a5c2f5c496775d1feb0027d7a99f5757f395
Mads <mads61096@gmail.com>
2021-12-06 13:36:10 +0100

    updateret selenium

--- BreathInDrinkUITest/UnitTest1.cs
+++ BreathInDrinkUITest/UnitTest1.cs
@@ -64,6 +64,15 @@ namespace BreathInDrinkUITest
 
             Thread.Sleep(2000);
 
+            IWebElement nameInput = _driver.FindElement(By.Id("inputName"));
+            nameInput.Clear();
+            nameInput.SendKeys("Mads");
+
+            IWebElement submit = _driver.FindElement(By.Id("SubmitButton"));
+            submit.Click();
+
+            Thread.Sleep(1000);
+
             double GetPromillle()
             {
                 Promille result = _context.Promille.ToList()[^1];
@@ -73,12 +82,37 @@ namespace BreathInDrinkUITest
             Assert.AreEqual("BreathNDrink", _driver.Title);
 
             //Checker måling knap og udskreven promille
-            IWebElement inputElement = _driver.FindElement(By.Id("GetMålingButton"));
+            IWebElement inputElement = _driver.FindElement(By.Id("GetAlchoholLevelButton"));
             inputElement.Click();
-            IWebElement outputElement = _driver.FindElement(By.Id("Promille"));
-            string textActual = outputElement.Text;
-            string textExpected = Math.Round(GetPromillle(), 1).ToString();
-            Assert.AreEqual(Math.Round(GetPromillle(), 1).ToString(CultureInfo.InvariantCulture), textActual);
+
+            double promille = GetPromillle();
+            if (promille < 0.7)
+            {
+                IWebElement outputElement = _driver.FindElement(By.Id("Promille1"));
+                string textActual = outputElement.Text;
+                Assert.AreEqual(Math.Round(GetPromillle(), 1).ToString(CultureInfo.InvariantCulture) + "‰", textActual);
+            }
+            else if (promille >= 0.7 && promille < 1.4)
+            {
+                IWebElement outputElement = _driver.FindElement(By.Id("Promille2"));
+                string textActual = outputElement.Text;
+                Assert.AreEqual(Math.Round(GetPromillle(), 1).ToString(CultureInfo.InvariantCulture) + "‰", textActual);
+            }
+            else if (promille >= 1.4 && promille < 2.9)
+            {
+                IWebElement outputElement = _driver.FindElement(By.Id("Promille3"));
+                string textActual = outputElement.Text;
+                Assert.AreEqual(Math.Round(GetPromillle(), 1).ToString(CultureInfo.InvariantCulture) + "‰", textActual);
+            }
+            else
+            {
+                IWebElement outputElement = _driver.FindElement(By.Id("Promille4"));
+                string textActual = outputElement.Text;
+                Assert.AreEqual(Math.Round(GetPromillle(), 1).ToString(CultureInfo.InvariantCulture) + "‰", textActual);
+            }
+
+            IWebElement filter = _driver.FindElement(By.Id("filterButton"));
+            filter.Click();
 
             //checker person input
             IWebElement weightElement = _driver.FindElement(By.Id("weightField"));
@@ -118,13 +152,27 @@ namespace BreathInDrinkUITest
 
             Thread.Sleep(1000);
 
+            IWebElement nameInput = _driver.FindElement(By.Id("inputName"));
+            nameInput.Clear();
+            nameInput.SendKeys("Mads");
+
+            IWebElement submit = _driver.FindElement(By.Id("SubmitButton"));
+            submit.Click();
+
+            Thread.Sleep(1000);
+
+            IWebElement inputElement = _driver.FindElement(By.Id("GetAlchoholLevelButton"));
+            inputElement.Click();
+
+            Thread.Sleep(1000);
+
             IWebElement showAllElement = _driver.FindElement(By.Id("showAllButton"));
             showAllElement.Click();
 
             IWebElement outputElement = _driver.FindElement(By.Id("DrinkList"));
             string text = outputElement.Text;
             Assert.IsTrue(text.Contains("GG"));
-            Assert.IsTrue(text.Contains("PromilleChange"));
+            Assert.IsTrue(text.Contains("Promille Change"));
         }
 
         [TestMethod]
@@ -138,6 +186,20 @@ namespace BreathInDrinkUITest
             _driver.SwitchTo().ActiveElement();
             IWebElement yesButton = _driver.FindElement(By.Id("yesButton"));
             yesButton.Click();
+            Thread.Sleep(1000);
+
+            IWebElement nameInput = _driver.FindElement(By.Id("inputName"));
+            nameInput.Clear();
+            nameInput.SendKeys("Mads");
+
+            IWebElement submit = _driver.FindElement(By.Id("SubmitButton"));
+            submit.Click();
+
+            Thread.Sleep(1000);
+
+            IWebElement measure = _driver.FindElement(By.Id("GetAlchoholLevelButton"));
+            measure.Click();
+
             Thread.Sleep(1000);
 
             IWebElement showAllElement = _driver.FindElement(By.Id("showAllButton"));
@@ -158,7 +220,7 @@ namespace BreathInDrinkUITest
             string modalMeasurements = measurementsOutput.Text;
             Assert.AreEqual("GG", modalDrinkName);
             Assert.IsTrue(modalIngredients.Contains("Galliano"));
-            Assert.AreEqual("Alkohol 42%", modalAlcohol);
+            Assert.AreEqual("Alcohol 42%", modalAlcohol);
             Assert.IsTrue(modalMeasurements.Contains("2 1/2"));
 
 
@@ -178,16 +240,34 @@ namespace BreathInDrinkUITest
             _driver.SwitchTo().ActiveElement();
             IWebElement yesButton = _driver.FindElement(By.Id("yesButton"));
             yesButton.Click();
+
             Thread.Sleep(1000);
 
-            IWebElement showAllElement = _driver.FindElement(By.Id("showAllButton"));
-            showAllElement.Click();
+            IWebElement nameInput = _driver.FindElement(By.Id("inputName"));
+            nameInput.Clear();
+            nameInput.SendKeys("Mads");
 
-            IWebElement vodkaElement = _driver.FindElement(By.Id("vodkaFilter"));
+            IWebElement submit = _driver.FindElement(By.Id("SubmitButton"));
+            submit.Click();
+
+            Thread.Sleep(3000);
+
+            IWebElement measure = _driver.FindElement(By.Id("GetAlchoholLevelButton"));
+            measure.Click();
+
+            Thread.Sleep(1000);
+
+            IWebElement filter = _driver.FindElement(By.Id("filterButton"));
+            filter.Click();
+
+            IWebElement vodkaElement = _driver.FindElement(By.Id("filterItem"));
             vodkaElement.Click();
 
-            IWebElement rumElement = _driver.FindElement(By.Id("rumFilter"));
-            rumElement.Click();
+            IWebElement maxPromille = _driver.FindElement(By.Id("alkoFilter2"));
+            maxPromille.Click();
+
+            IWebElement recommendElement = _driver.FindElement(By.Id("recommendButton"));
+            recommendElement.Click();
 
             IWebElement outputElement = _driver.FindElement(By.Id("DrinkList"));
             string text = outputElement.Text;
@@ -196,6 +276,23 @@ namespace BreathInDrinkUITest
             IWebElement outputElement2 = _driver.FindElement(By.Id("DrinkList"));
             string text2 = outputElement2.Text;
             Assert.IsTrue(text2.Contains("Aztec Punch"));
+
+            IWebElement reset = _driver.FindElement(By.Id("resetButton"));
+            reset.Click();
+
+            IWebElement notFilter = _driver.FindElement(By.Id("filterNotItem"));
+            notFilter.Click();
+
+            Thread.Sleep(5000);
+
+            recommendElement.Click();
+
+            Thread.Sleep(4000);
+
+            IWebElement outputElement3 = _driver.FindElement(By.Id("DrinkList"));
+            string text3 = outputElement3.Text;
+            Assert.IsTrue(text3.Contains("GG"));
+
         }
     }
 }
